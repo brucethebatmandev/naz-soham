2532fcbb0081aa0cbc59151e27b1a8a0e839d30f
Bruce Wayne <brucethebatmandev@gmail.com>
2024-07-23 12:44:50 +0100

    updating contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,35 +1,28 @@
 const Contact = () => {
-    return (
-//   This example requires some changes to your config:
-  
-//   ```
-//   // tailwind.config.js
-//   module.exports = {
-//     // ...
-//     plugins: [
-//       // ...
-//       require('@tailwindcss/forms'),
-//     ],
-//   }
-//   ```
+  return (
+    //   This example requires some changes to your config:
 
+    //   ```
+    //   // tailwind.config.js
+    //   module.exports = {
+    //     // ...
+    //     plugins: [
+    //       // ...
+    //       require('@tailwindcss/forms'),
+    //     ],
+    //   }
+    //   ```
 
-<div className="mt-20 mb-20 flex flex-col">
+    <div className="grid lg:grid-cols-2 lg:mb-20">
 
-  <div className="flex flex-col items-center mb-5 lg:mb-20">
-    <h1 className="text-6xl">Contact Us</h1>
-  </div>
+      {/* Phone, Email, Address & Opening Hours*/}
+      <div className="grid gap-4 justify-center items-center bg-[#fafaed] h-full rounded-md tracking-wide">
 
-  <div className="grid grid-cols-1 lg:grid-cols-2">
-    
-    
-    <div className="flex flex-col justify-center md:bg-cover md:bg-center mx-8 lg:mx-12 rounded-md">
+        <h2 className="text-3xl">How to reach out</h2>
 
-      <div className="flex flex-col gap-4 justify-center items-center relative bg-[#fafaed] h-full rounded-md py-12 tracking-wide">
-        
-        <div className="flex flex-col gap-3">          
+        <div className="grid gap-3">
 
-          {/* Phone */} 
+          {/* Phone */}
           <div className="flex gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
               stroke="currentColor" className="w-6 h-6">
@@ -48,14 +41,14 @@ const Contact = () => {
               <path strokeLinecap="round" strokeLinejoin="round"
                 d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
             </svg>
-            <div className="flex flex-col items-center justify-center">
+            <div className="grid items-center justify-center">
               <a href="" className="hover:cursor-pointer">
                 <p>info@nazsoham.co.uk</p>
               </a>
-            </div> 
+            </div>
           </div>
-        
-          {/* Address */} 
+
+          {/* Address */}
           <div className="flex gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
               stroke="currentColor" className="w-6 h-6">
@@ -72,10 +65,10 @@ const Contact = () => {
 
 
         {/* <!-- Opening Hours --> */}
-        <div className="mx-auto w-56">
-          <h2 className="mt-8 mb-2  text-lg">Opening Hours</h2>
+        <div className="_mx-auto w-56">
+          <h2 className="mt-8 mb-2 text-lg">Opening Hours</h2>
 
-          <div className="">
+          <div>
             <p className="flex justify-between gap-3">
               <span>Mon:</span>
               <span>5pm - 10pm</span>
@@ -109,59 +102,74 @@ const Contact = () => {
 
       </div>
 
+      {/* <!-- Contact Form --> */}
+      <div className="mb-20 lg:mb-0">
+
+        <div className="mt-3 sm:mt-20 lg:mt-0 xl:mt-0 px-10 md:px-0 mx-auto w-full max-w-lg lg:px-8">
+
+
+          <h2 className="text-3xl mb-4">Send us a Message</h2>
+          <form
+            action="#"
+            method="POST"
+
+          >
+            <div className="grid gap-x-8 gap-y-6 sm:grid-cols-2 text-gray-900">
+
+              <div>
+                <label className="block text-sm leading-6">First name</label>
+                <div className="mt-2.5">
+                  <input type="text" name="first-name" id="first-name" autoComplete="given-name"
+                    className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div>
+                <label className="block text-sm leading-6">Last name</label>
+                <div className="mt-2.5">
+                  <input type="text" name="last-name" id="last-name" autoComplete="family-name"
+                    className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label className="block text-sm leading-6">Email</label>
+                <div className="mt-2.5">
+                  <input type="email" name="email" id="email" autoComplete="email"
+                    className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label className="block text-sm leading-6">Message</label>
+                <div className="mt-2.5">
+                  <textarea name="message" id="message"
+                    // rows="6"
+                    className="block w-full border-1 px-3.5 py-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"></textarea>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="mt-10">
+              <button type="submit"
+                className="block w-full px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 border-double border-4 border-slate-700 hover:border-slate-600">Send
+                Message</button>
+            </div>
+          </form>
+
+        </div>
+
+
+
+      </div>
+
+
     </div>
 
 
-    {/* <!-- Contact Form --> */}
-    <form action="#" method="POST"
-      className="mx-auto mt-3 xl:mt-0 max-w-2xl sm:mt-20 lg:mt-0 px-10 md:px-0 w-full">
-      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2 text-gray-900">
 
-        <div>
-          <label className="block text-sm font-semibold leading-6">First name</label>
-          <div className="mt-2.5">
-            <input type="text" name="first-name" id="first-name" autoComplete="given-name"
-              className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div>
-          <label className="block text-sm font-semibold leading-6">Last name</label>
-          <div className="mt-2.5">
-            <input type="text" name="last-name" id="last-name" autoComplete="family-name"
-              className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div className="sm:col-span-2">
-          <label className="block text-sm font-semibold leading-6">Email</label>
-          <div className="mt-2.5">
-            <input type="email" name="email" id="email" autoComplete="email"
-              className="block w-full border-1 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"/>
-          </div>
-        </div>
-
-        <div className="sm:col-span-2">
-          <label className="block text-sm font-semibold leading-6">Message</label>
-          <div className="mt-2.5">
-            <textarea name="message" id="message"  
-            // rows="6"
-              className="block w-full border-1 px-3.5 py-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"></textarea>
-          </div>
-        </div>
-
-      </div>
-
-      <div className="mt-10">
-        <button type="submit"
-          className="block w-full px-3.5 py-2.5 text-center text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 border-double border-4 border-slate-700 hover:border-slate-600">Send
-          Message</button>
-      </div>
-    </form>
-
-  </div>
-</div>
-    )
+  )
 }
 
 export default Contact
